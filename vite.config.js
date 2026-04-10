cbc1e34dc7f506673ef8b5248de34dd457d9be07
Maximiliano Pulido Beltran <maximiliano.pulido@blend360.com>
2026-04-10 18:00:56 -0500

    feat: deploy to GitHub Pages with custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
-    host: '0.0.0.0',
-    allowedHosts: ['all'],
-  },
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
-    host: '0.0.0.0',
-  },
+  base: '/',
 })
